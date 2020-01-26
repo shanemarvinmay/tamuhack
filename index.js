e4e93f9bf503159e1efc01f496bbc2a4f563875d
CesarRiv <riveracesar82@gmail.com>
2020-01-25 22:25:32 -0600

    getting  ready to merge with shane

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+async function quickstart() { 
+    //imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+    
+    //Creates a client 
+    const client = new vision.ImageAnnotatorClient();
+
+    //Performs label detection on the image file 
+    const [result] = await client.labelDetection("ZeroTwo.jpg");
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+}
+// quickstart();
+
 app.get('/', (req, res) => res.send('Tamuhack!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
